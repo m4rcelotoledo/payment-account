1e6a9343a4babefe42eedd0a78b12037085b202f
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-04-13 15:31:59 -0300

    Setup FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,4 +34,6 @@ RSpec.configure do |config|
   config.use_active_record = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.include FactoryBot::Syntax::Methods
 end
