1606dd40b7151e1fe621a570abf7c240b97c45a8
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-04-13 17:38:21 -0300

    Fix Rubocop offenses

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe 'Transactions' do
     context 'when payment method is Pix' do
       it 'deducts exact amount and returns 201' do
         post '/transaction', params: {
-          transaction: { payment_method: 'P', account_number: account.account_number, amount: 10.0 } }
+          transaction: { payment_method: 'P', account_number: account.account_number, amount: 10.0 }
+        }
 
         expect(response).to have_http_status(:created)
         expect(response.parsed_body['balance']).to eq('170.37')
@@ -19,7 +20,8 @@ RSpec.describe 'Transactions' do
     context 'when payment method is Debit' do
       it 'deducts amount plus 3% fee and returns 201' do
         post '/transaction', params: {
-          transaction: { payment_method: 'D', account_number: account.account_number, amount: 10.0 } }
+          transaction: { payment_method: 'D', account_number: account.account_number, amount: 10.0 }
+        }
 
         expect(response).to have_http_status(:created)
         expect(response.parsed_body['balance']).to eq('170.07')
@@ -29,7 +31,8 @@ RSpec.describe 'Transactions' do
     context 'when payment method is Credit' do
       it 'deducts amount plus 5% fee and returns 201' do
         post '/transaction', params: {
-          transaction: { payment_method: 'C', account_number: account.account_number, amount: 10.0 } }
+          transaction: { payment_method: 'C', account_number: account.account_number, amount: 10.0 }
+        }
 
         expect(response).to have_http_status(:created)
         expect(response.parsed_body['balance']).to eq('169.87')
@@ -39,7 +42,8 @@ RSpec.describe 'Transactions' do
     context 'when balance is insufficient' do
       it 'returns 404' do
         post '/transaction', params: {
-          transaction: { payment_method: 'P', account_number: account.account_number, amount: 999.0 } }
+          transaction: { payment_method: 'P', account_number: account.account_number, amount: 999.0 }
+        }
 
         expect(response).to have_http_status(:not_found)
       end
@@ -56,7 +60,8 @@ RSpec.describe 'Transactions' do
     context 'when payment method is invalid' do
       it 'returns 404' do
         post '/transaction', params: {
-          transaction: { payment_method: 'X', account_number: account.account_number, amount: 10.0 } }
+          transaction: { payment_method: 'X', account_number: account.account_number, amount: 10.0 }
+        }
 
         expect(response).to have_http_status(:not_found)
       end
